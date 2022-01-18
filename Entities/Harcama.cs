b5a1317319c99a8c1fd3bce636f08e89c04778f4
abidinyarata <abidinyarata@windowslive.com>
2022-01-18 10:50:20 +0300

    Harcama entity class ve HarcamaTipi enumu eklendi

---
+++ Entities/Harcama.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Harcama
+    {
+        public int Id { get; set; }
+        public HarcamaTipi Tip { get; set; }
+        public string Aciklama { get; set; }
+        public decimal Tutar { get; set; }
+        public DateTime Tarihi { get; set; }
+        public HarcamaOnayDurumu Durumu { get; set; }
+
+        public virtual Personel Personel { get; set; }
+    }
+
+    public enum HarcamaTipi
+    {
+        Yol,
+        Yemek,
+        Diger,
+        //....
+    }
+
+    public enum HarcamaOnayDurumu
+    {
+        Beklemede,
+        Kabul,
+        Red
+    }
+
+}

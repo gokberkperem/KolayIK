8fa8e094d59126198acb6f9efea17c3e6c6cabd0
Gökberk Perem <gokberk.perem@prodyum.com>
2022-01-20 21:39:30 +0300

    Sirket tablosu güncellendi

---
+++ DataAccess/Migrations/20220120182241_sirketTablosuParolaEkleme.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class sirketTablosuParolaEkleme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Sirketler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Parola",
+                table: "Sirketler",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ParolaTekrari",
+                table: "Sirketler",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Parola",
+                table: "Sirketler");
+
+            migrationBuilder.DropColumn(
+                name: "ParolaTekrari",
+                table: "Sirketler");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Sirketler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
